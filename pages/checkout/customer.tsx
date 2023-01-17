17970fe5fbfcc2f11b8ad9ec93246b9554492b1c
Armando Murillo <totovip1464@gmail.com>
2023-01-17 15:15:56 -0600

    Cambio el ShopLayout por el FormCustomerLayout

--- pages/checkout/customer.tsx
+++ pages/checkout/customer.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 
 import { Typography, Box } from '@mui/material';
 
-import { ShopLayout } from "../../components/layout";
+import { FormCustomerLayout } from "../../components/layout";
 import { FormCustomer } from '../../components/sections/CustomerInformation/FormCustomer';
 
 
@@ -10,7 +10,7 @@ import { FormCustomer } from '../../components/sections/CustomerInformation/Form
 const CustomerInformationPage: NextPage = () => {
 
     return (
-        <ShopLayout title="Información del cliente" pageDescription="Aqui el cliente colocará su información para el pedido">
+        <FormCustomerLayout title="Información del cliente" pageDescription="Aqui el cliente colocará su información para el pedido">
 
             <Box sx={{ mt: 15 }} height='calc(100vh - 200px)' className='containers cardForm-container'>
                 <Typography width='100%' variant="h1" component='h1' sx={{ display: 'flex', justifyContent: 'center', color: 'white', pt: 10, fontSize: 45 }}>Información para el pedido</Typography>  
@@ -18,7 +18,7 @@ const CustomerInformationPage: NextPage = () => {
                 <FormCustomer />   
             </Box>
 
-        </ShopLayout>
+        </FormCustomerLayout>
     )
 
 }
